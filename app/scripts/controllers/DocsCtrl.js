90053b3273af810528158bb8ae14d1f294743f4e
notdryft <gui.corre@gmail.com>
2016-01-11 17:13:02 +0100

    Handle talks properly

--- app/scripts/controllers/DocsCtrl.js
+++ app/scripts/controllers/DocsCtrl.js
@@ -14,19 +14,14 @@ var DocsCtrl = function ($scope) {
   //                   Scope variables                    //
   //------------------------------------------------------//
 
-  $scope.talks = [
-    'TALK_1',  'TALK_2',  'TALK_3',
-    'TALK_4',  'TALK_5',  'TALK_6',
-    'TALK_7',  'TALK_8',  'TALK_9',
-    'TALK_10', 'TALK_11', 'TALK_12',
-    'TALK_13', 'TALK_14', 'TALK_15',
-    'TALK_16', 'TALK_17', 'TALK_19',
-    'TALK_20', 'TALK_21', 'TALK_22',
-    'TALK_23', 'TALK_24', 'TALK_25',
-    'TALK_26'
-  ];
+  var MAX = 50; // FIXME Move in a constant factory
 
+  var talks = [];
+  for (var i = MAX; i > 0; i--) {
+    talks.push('TALK_' + i);
+  }
 
+  $scope.talks = talks;
 };
 
 angular.module('gatling.io').controller('DocsCtrl', ['$scope', '$location', DocsCtrl]);
